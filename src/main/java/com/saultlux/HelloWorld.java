5fbcd7354044a5b0f85b38e3227e88337665161d
MY <MY@DESKTOP-OPUB36N>
2021-03-08 16:40:44 +0900

    첫 시작!

---
+++ src/main/java/com/saultlux/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.saultlux;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+		System.out.println("Hellow World");
+		System.out.println("Hellow World");
+		System.out.println("Hellow World");
+		System.out.println("Hellow World");
+	}
+
+}
